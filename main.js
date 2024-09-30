64c9f6828a6a176dd536b9d4755dec1bb7eb3695
Roberto Lacava <roberto.lacava@gmail.com>
2024-09-30 13:15:16 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Crea 5 variabili contenenti un numero e scrivi un programma in modo da ottenere la somma tra questi numeri e la media.
+// In console poi mostra la seguente frase: ‘La somma tra i numeri equivale a … e la media equivale a…
+
+let n1 = 53;
+let n2 = 32;
+let n3 = 19;
+let n4 = 83;
+let n5 = 7;
+
+console.log(`I numeri scelti sono ${n1} ${n2} ${n3} ${n4} ${n5}` );
+
+// somma
+let somma = n1+n2+n3+n4+n5;
+console.log("La somma di tutti i numeri è: "+somma);
+
+
+// media
+let media = somma/5
+console.log("La media dei numeri è: "+media);
